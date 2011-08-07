f0d58c35233ba4b41ac0e8d933ea9f8256988e0e
Michael Dillon <michaelcdillon@me.com>
2011-08-07 16:10:58 -0400

    small hello world to verify build script

---
+++ src/org/bca/Main.java
@@ -0,0 +1,11 @@
+package org.bca;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static Logger log = Logger.getLogger (Main.class.getName ());
+
+    public static void main (String[] args) {
+        log.info ("hello World"); 
+    }
+}
